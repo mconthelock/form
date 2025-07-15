0b99979bca37edc7f104402f6c2ff972c1df5d1c
Onthelock <s.chalormsak@gmail.com>
2025-07-15 18:58:44 +0700

    fix: old version

--- assets/script/public/v1.0.2/component/sidebar.js
+++ assets/script/public/v1.0.2/component/sidebar.js
@@ -1,46 +1,44 @@
 import { host } from "../jFuntion";
 import "../../../../dist/css/sidebar.min.css";
-import "../_tooltip"
+import "../_tooltip";
 
-$(document).on('click', '#sidebarToggle', function () {
-    // $('#sidebar').toggleClass('collapsed');
-    if ($('#sidebar').hasClass('collapsed')) {
-        $('#sidebar').removeClass('collapsed');
-        $('#sidebarToggle').attr('data-html', 'Collapse menu');
-    } else if ($('#sidebar').hasClass('collapsed-hover')) {
-        iconMenu();
-        $('#sidebar').removeClass('collapsed-hover collapsed');
-        $('#sidebarToggle').attr('data-html', 'Collapse menu');
-    } else {
-        $('#sidebar').addClass('collapsed');
-        $('#sidebarToggle').attr('data-html', 'Expand menu');
-    }
+$(document).on("click", "#sidebarToggle", function () {
+  // $('#sidebar').toggleClass('collapsed');
+  if ($("#sidebar").hasClass("collapsed")) {
+    $("#sidebar").removeClass("collapsed");
+    $("#sidebarToggle").attr("data-html", "Collapse menu");
+  } else if ($("#sidebar").hasClass("collapsed-hover")) {
+    iconMenu();
+    $("#sidebar").removeClass("collapsed-hover collapsed");
+    $("#sidebarToggle").attr("data-html", "Collapse menu");
+  } else {
+    $("#sidebar").addClass("collapsed");
+    $("#sidebarToggle").attr("data-html", "Expand menu");
+  }
 });
 
-
-
-$(document).on('mouseover', '#sidebar #menu, #sidebar #profile', function(){
-    if($('#sidebar').hasClass('collapsed')){
-        $('#sidebarToggle').html(`<i class="icofont-tack-pin text-xl"></i>`);
-        $('#sidebarToggle').attr('data-html', 'Keep menu open');
-        $('#sidebar').removeClass('collapsed').addClass('collapsed-hover');
-    }
+$(document).on("mouseover", "#sidebar #menu, #sidebar #profile", function () {
+  if ($("#sidebar").hasClass("collapsed")) {
+    $("#sidebarToggle").html(`<i class="icofont-tack-pin text-xl"></i>`);
+    $("#sidebarToggle").attr("data-html", "Keep menu open");
+    $("#sidebar").removeClass("collapsed").addClass("collapsed-hover");
+  }
 });
 
-$(document).on('mouseleave', '#sidebar', function(){
-    if($('#sidebar').hasClass('collapsed-hover')){
-        iconMenu();
-        $('#sidebarToggle').attr('data-html', 'Expand menu');
-        $('#sidebar').removeClass('collapsed-hover').addClass('collapsed');
-    }
+$(document).on("mouseleave", "#sidebar", function () {
+  if ($("#sidebar").hasClass("collapsed-hover")) {
+    iconMenu();
+    $("#sidebarToggle").attr("data-html", "Expand menu");
+    $("#sidebar").removeClass("collapsed-hover").addClass("collapsed");
+  }
 });
 
 function iconMenu() {
-    $('#sidebarToggle').html(`<i class="icofont-navigation-menu text-xl"></i>`);
+  $("#sidebarToggle").html(`<i class="icofont-navigation-menu text-xl"></i>`);
 }
 
 export function initSidebar(options) {
-    const sidebar = `
+  const sidebar = `
         <div id="sidebar" class="menu transition-all w-full md:w-64 lg:w-80 min-h-full bg-primary text-base-100 text-base pt-1" >
             <div class="flex items-center sidebar-head px-4 py-2 gap-3">
                 <div tabindex="0" role="button" class="sidebar-logo btn btn-ghost btn-circle bg-gray-50 w-12 h-12">
@@ -58,53 +56,54 @@ export function initSidebar(options) {
             <div id="menu"></div>
             <div id="profile" class="mt-auto"></div>
         </div>
-    `
-    // $('#sidebar').html(sidebar);
-    $('#sidebar').replaceWith(sidebar);
-};
-
+    `;
+  // $('#sidebar').html(sidebar);
+  $("#sidebar").replaceWith(sidebar);
+}
 
 /**
  * set sidebar menu and profile
- * @param {object} menu 
- * @param {object} info 
+ * @param {object} menu
+ * @param {object} info
  */
-export async function setSidebarMenu(menu, info){
-    let listMenu = '';
-    // sidebar menu
-    if(menu.length > 0){
-        menu.forEach(m => {
-            if(m.submenu){
-                listMenu += `<li class="mainmenu ${m.menu_class} px-6">
+export async function setSidebarMenu(menu, info) {
+  let listMenu = "";
+  // sidebar menu
+  if (menu.length > 0) {
+    menu.forEach((m) => {
+      if (m.submenu) {
+        listMenu += `<li class="mainmenu ${m.menu_class} px-6">
                                 <details>
                                     <summary class="font-semibold text-base">
                                         <span class="text-2xl">${m.menu_icon}</span>
                                         <span class='sidebar-text'>${m.menu_name}</span>
                                     </summary>
                                     <ul class="text-sm list-disc">`;
-                m.submenu.forEach(sub => {
-                    listMenu += `<li class="${sub.menu_class}"><a href="${host}}/${sub.menu_link}" class="menu-name sidebar-text">${sub.menu_name}</a></li>`;
-                });
-                listMenu += `</ul></details></li>`;
-            }else{
-                listMenu += `<li class="${m.menu_class} px-6">
+        m.submenu.forEach((sub) => {
+          listMenu += `<li class="${sub.menu_class}"><a href="${host}}/${sub.menu_link}" class="menu-name sidebar-text">${sub.menu_name}</a></li>`;
+        });
+        listMenu += `</ul></details></li>`;
+      } else {
+        listMenu += `<li class="${m.menu_class} px-6">
                                 <a href="${host}/${m.menu_link}" class="sidebar-link">
                                     <span class="text-2xl">${m.menu_icon}</span>
                                     <span class="font-semibold menu-name sidebar-text">${m.menu_name}</span>
                                 </a>
                             </li>`;
-            }
-        });
-        $('#menu').html(listMenu);
-    }
+      }
+    });
+    $("#menu").html(listMenu);
+  }
 
-    if (info) {
-        const fullname = info.SNAME.split(' ');
-        const name = fullname[0];
-        const profileImg = info.image ? info.image : `${host}/assets/img/avatar.png`;
-        
-        // sidebar profile
-        $('#profile').html(`
+  if (info) {
+    const fullname = info.SNAME.split(" ");
+    const name = fullname[0];
+    const profileImg = info.image
+      ? info.image
+      : `${host}/assets/img/avatar.png`;
+
+    // sidebar profile
+    $("#profile").html(`
             <li class="">
                 <hr>
                 <div class="flex sidebar-profile">
@@ -128,18 +127,18 @@ export async function setSidebarMenu(menu, info){
                 </div>
             </li>
         `);
-        // navbar profile
-        $('#nav-profile').html(`<img alt="${info.SNAME}" src="${profileImg}" />`) 
-    }
-    menuFocus();
+    // navbar profile
+    $("#nav-profile").html(`<img alt="${info.SNAME}" src="${profileImg}" />`);
+  }
+  menuFocus();
 }
 
-export function menuFocus(){
-    if ($("body").attr("menuTitle")) {
-        const title = $("body").attr("menuTitle");
-        const menu = $(`.${title}`);
-        // console.log(title, menu);
-        
-        menu.find("a").addClass("menu-focus");
-    }
+export function menuFocus() {
+  if ($("body").attr("menuTitle")) {
+    const title = $("body").attr("menuTitle");
+    const menu = $(`.${title}`);
+    // console.log(title, menu);
+
+    menu.find("a").addClass("menu-focus");
+  }
 }
