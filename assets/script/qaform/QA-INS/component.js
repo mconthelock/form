b5f4cf0164219ab637558c2721c7ecb570c95ac5
sutthipongjab <sutthipongjab@gmail.com>
2025-09-10 10:47:21 +0700

    feat: add factor and max score in master qains

--- assets/script/qaform/QA-INS/component.js
+++ assets/script/qaform/QA-INS/component.js
@@ -1,13 +1,21 @@
 const btnStatus = ({
-    cls = 'list-status',
+    cls = "list-status",
     status = 1,
-    attr = ''
+    attr = "",
 } = {}) => `<button class="btn  flex items-center gap-2 ${cls}" ${attr}>
         Status
-        <input type="checkbox" ${status == 1 ? 'checked="checked"' : ""} class="toggle toggle-xl toggle-success " />
+        <input type="checkbox" ${
+            status == 1 ? 'checked="checked"' : ""
+        } class="toggle toggle-xl toggle-success " />
     </button>`;
-const btnDel = ({cls = '', text = 'Delete', attr = ''} = {}) => `<button class="btn btn-error ${cls}" ${attr}><i class="icofont-ui-delete"></i>${text}</button>`;
-const btnAdd = ({cls = '', text = '', attr = ''} = {}) => `<button class="${cls} btn" ${attr}><i class="icofont-ui-add"></i>${text}</button>`;
-const input = ({val="", cls = '', type = 'text', attr = ''} = {}) => `<input type="${type}" class="input w-full text-black ${cls}" value="${val}" ${attr} />`;
+
+const btnDel = ({ cls = "", text = "Delete", attr = "" } = {}) =>
+    `<button class="btn btn-error ${cls}" ${attr}><i class="icofont-ui-delete"></i>${text}</button>`;
+
+const btnAdd = ({ cls = "", text = "", attr = "" } = {}) =>
+    `<button class="${cls} btn" ${attr}><i class="icofont-ui-add"></i>${text}</button>`;
+
+const input = ({ val = "", cls = "", type = "text", attr = "" } = {}) =>
+    `<input type="${type}" class="input w-full text-black ${cls}" value="${val}" ${attr} />`;
 
 export { btnStatus, btnDel, btnAdd, input };
