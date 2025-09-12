4b9ab28419f85280cca86a96883edd8ad151d6ad
sutthipongjab <sutthipongjab@gmail.com>
2025-09-12 13:58:26 +0700

    feat(qains): create master preview page

--- application/views/qaform/QA-INS/preview.blade.php
+++ application/views/qaform/QA-INS/preview.blade.php
@@ -3,12 +3,13 @@
 @section('contents')
 <div class="secid" secId="{{$secId}}"></div>
 <div class="flex flex-col gap-5">
-    <div class="flex flex-col gap-5 mb-5 p-5 bg-white rounded-[3px] shadow w-full h-full border-t-3 border-[#3c8dbc]">
-        <u class="flex flex-col items-center">
+    <div class="flex flex-col gap-5 p-5 bg-white rounded-[3px] shadow w-full h-full border-t-3 border-[#3c8dbc] relative">
+        <u class="flex flex-col items-center mb-5">
             <span class="text-2xl font-bold">Quality Built In Line Audit Report</span>
             <span class="text-2xl font-bold">Strengthen Trouble Report After Shipment</span>
             <span class="text-2xl font-bold" id="revision" rev="{{$rev}}">Revision {{$revText}}</span>
         </u>
+        <div id="score"></div>
         <div id="auditReport"></div>
     </div>
 </div>
