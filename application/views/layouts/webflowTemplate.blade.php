7e20842b24bc3baf32c7918eeb8160bb9478eebc
Chalormsak S. <s.chalormsak@gmail.com>
2026-01-16 17:25:21 +0700

    fix: correct modal z-index class syntax in webflowTemplate

--- application/views/layouts/webflowTemplate.blade.php
+++ application/views/layouts/webflowTemplate.blade.php
@@ -17,7 +17,6 @@
     <link rel="apple-touch-icon" href="{{ base_url() }}assets/images/favicon.ico">
     <link rel="apple-touch-startup-image" href="{{ base_url() }}assets/images/icon_512.png">
     <title>AMEC Webflow</title>
-
     <link rel="stylesheet" href="{{ $_ENV['APP_CDN'] }}/icofont/icofont.min.css">
     <link rel="stylesheet" href="{{ base_url() }}assets/dist/css/tailwind.css?ver={{ $GLOBALS['version'] }}">
     @yield('styles')
@@ -25,7 +24,7 @@
 
 <body class="flex flex-col min-h-screen">
     <input type="checkbox" id="loading-box" class="modal-toggle" />
-    <div class="modal !z-[9999]" role="dialog">
+    <div class="modal z-9999!" role="dialog">
         <div class="loader"></div>
     </div>
 
