44d4f3ea72e7c3b6e91275aa8e2b86a08fd0faae
Supamid <supamid@MitsubishiElevatorAsia.co.th>
2026-02-25 15:28:31 +0700

    feat: implement cascade delete for lines and stops, enhancing data integrity

--- assets/script/gpform/BUS/data.js
+++ assets/script/gpform/BUS/data.js
@@ -25,10 +25,12 @@ export const getAllEmp   = (data) => callAPI("/bus/passenger/getAllTransport", "
 export const insertLine  = (data) => callAPI("/bus/line/create", "POST", data);
 export const updateLine  = (data) => callAPI("/bus/line/update", "POST", data);
 export const deleteLine  = (data) => callAPI("/bus/line/delete", "POST", data);
+export const deleteLineCascade  = (data) => callAPI("/bus/line/deleteCascade", "POST", data);
 
 export const insertStop  = (data) => callAPI("/bus/stop/create", "POST", data);
 export const updateStop  = (data) => callAPI("/bus/stop/update", "POST", data);
 export const deleteStop  = (data) => callAPI("/bus/stop/delete", "POST", data);
+export const deleteStopandPassenger  = (data) => callAPI("/bus/stop/deleteStop", "POST", data);
 
 export const insertRoute  = (data) => callAPI("/bus/route/create", "POST", data);
 export const updateRoute  = (data) => callAPI("/bus/route/update", "POST", data);
