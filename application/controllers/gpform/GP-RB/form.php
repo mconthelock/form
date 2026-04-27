09eb8df17377837e47c8b9cb99475816008f0cdb
Chalormsak S. <s.chalormsak@gmail.com>
2026-04-27 10:38:39 +0700

    feat: add from GP-RB

---
+++ application/controllers/gpform/GP-RB/form.php
@@ -0,0 +1,19 @@
+<?php
+use GuzzleHttp\Client;
+defined('BASEPATH') OR exit('No direct script access allowed');
+require_once APPPATH.'controllers/api/webform/form.php';
+require_once APPPATH.'controllers/api/webform/flow.php';
+require_once APPPATH.'controllers/api/webform/formmst.php';
+
+class form extends MY_Controller{
+    use formApi, flow, formmst;
+    protected $client;
+    function __construct(){
+		parent::__construct();
+        $this->client = new Client(['verify' => false]);
+    }
+
+    public function main(){
+
+    }
+}
