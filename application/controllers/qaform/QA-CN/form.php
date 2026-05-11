2bc9005168352dd1e26542f1c76f4cec1d999d24
ksatidpaisankul <ksatidpaisankul@gmail.com>
2026-05-11 11:28:53 +0700

    change pic

--- application/controllers/qaform/QA-CN/form.php
+++ application/controllers/qaform/QA-CN/form.php
@@ -253,6 +253,8 @@ class form extends MY_Controller{
     public function action()
     {
         $act = $_POST["action"];
+        echo $act;
+        exit;
         $cextData = intval($_POST["cextData"]);
         $apvno =  $_POST["empno"];
         $nfrmno = $_POST["nfrmno"];
