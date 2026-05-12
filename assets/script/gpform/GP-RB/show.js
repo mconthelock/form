b64459845b04376dd6431d50644e5f1c61804208
punnawichs <punnawichs@mitsubishielevatorasia.co.th>
2026-05-12 14:07:32 +0700

    fix show form

--- assets/script/gpform/GP-RB/show.js
+++ assets/script/gpform/GP-RB/show.js
@@ -1,61 +1,49 @@
 import { fetchUtils } from "@amec/webasset/api/fetch-utils";
+import { getFormDetail } from "@amec/webasset/api/webform";
 
 $(async function () {
-    const data = {
-        "INPUTBY": "24011",
-        "REQBY": "24011",
-        "empName": "ชุติพงศ์ พลานนท์",
-        "empDept": "AAS SEC./IS DEPT./E/P DIV.",
-        "empPos": "STAFF",
-        "chkPurpose": 2
+  const tset = await getFormDetail("GP-RB");
 
-    }; /*เอามาจาก bcackend */
-    /*เรียกใช้ข้อมูลทีละตัว*/
-    const purpose = await getData();
-    console.log(purpose);
-    const Purposedata = purpose
-        .map((a) => {
-            const otherSelect = `<input type="text"
+
+  const showData = await getShowData();
+
+  /*เรียกใช้ข้อมูลทีละตัว*/
+  const purpose = await getData();
+  console.log(purpose);
+  const Purposedata = purpose
+    .map((a) => {
+      const otherSelect = `<input type="text"
                             class="input input-sm input-ghost w-full rounded-none border-b border-base-300 focus:border-primary focus:bg-base-200/50 px-1"
                             id="otherSelect" name="PURPOSE_OTHER" placeholder="Please specify other purpose" disabled readeonly>`;
 
-            return `<label class="flex items-center space-x-2 cursor-pointer">
+      return `<label class="flex items-center space-x-2 cursor-pointer">
                                 <input type="radio" name="PURPOSE_ID" 
                                     class="radio radio-xs rounded border-base-content [--chkbg:var(--bc)] [--chkfg:var(--b1)] req" value="${a.PURPOSE_ID}"
-                                    id="purpose_${a.PURPOSE_ID}" ${a.PURPOSE_ID==data.chkPurpose ? "checked" : ""}>
+                                    id="purpose_${a.PURPOSE_ID}" ${a.PURPOSE_ID == data.chkPurpose ? "checked" : ""}>
                                 <span>${a.PURPOSE_TH}/${a.PURPOSE_EN}</span>
                                 ${a.PURPOSE_ID == 4 ? otherSelect : ""} 
                             </label>`;
-        })
-        .join("");
+    })
+    .join("");
 
-    $('#INPUTBY').val(data.INPUTBY);
-    $('#REQBY').val(data.REQBY);
-    $('#empName').val(data.empName);
-    $('#empDept').val(data.empDept);
-    $('#empPos').val(data.empPos);
-    $("#purposeList").html(Purposedata);
-
+  $("#INPUTBY").val(data.INPUTBY);
+  $("#REQBY").val(data.REQBY);
+  $("#empName").val(data.empName);
+  $("#empDept").val(data.empDept);
+  $("#empPos").val(data.empPos);
+  $("#purposeList").html(Purposedata);
 });
 
 async function getData() {
-    return await fetchUtils({
-        url: `${process.env.APP_API}/gpform/gp-rb`,
-        method: "GET",
-    });
+  return await fetchUtils({
+    url: `${process.env.APP_API}/gpform/gp-rb`,
+    method: "GET",
+  });
 }
 
-async function getEmpData(empno) {
-    return await fetchUtils({
-        url: `${process.env.APP_API}/users/${empno}`,
-        method: "GET",
-    });
-}
-
-async function createForm(data) {
-    return fetchUtils({
-        url: `${process.env.APP_API}/gpform/gp-rb`,
-        method: "POST",
-        data: data,
-    });
+async function getShowData() {
+  return await fetchUtils({
+    url: `${process.env.APP_API}/gpform/show-gp-rb`,
+    method: "GET",
+  });
 }
