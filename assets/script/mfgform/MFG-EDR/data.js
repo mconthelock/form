e1a44497032e7700cc769195b4f9b5343d2b55dc
Supamid <supamid@MitsubishiElevatorAsia.co.th>
2026-05-14 08:45:38 +0700

    mfg edr 14/05/2026

--- assets/script/mfgform/MFG-EDR/data.js
+++ assets/script/mfgform/MFG-EDR/data.js
@@ -8,6 +8,7 @@ export const getamecorderdetail   = (data) => callAPI("/mfg-edr/amec-order-detai
 export const createMfgEdr = (data) => callAPI("/mfg-edr", "POST", data);
 export const updateMfgEdrDetail = (data) => callAPI("/mfg-edr/update-detail", "POST", data);
 export const getmfgedr   = (data) => callAPI("/mfg-edr/get-mfg-edr", "POST", data);
+export const updateWhyEffect = (data) => callAPI("/mfg-edr/update-why-effect", "POST", data);
 
 // ===== GET ===========
 export const getUserbyemp = (empno) => callAPI(`/users/${encodeURIComponent(empno)}`, "GET");
