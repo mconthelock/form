9018938887905567223ae59e75d026c02414f527
punnawichs <punnawichs@mitsubishielevatorasia.co.th>
2026-05-22 14:24:23 +0700

    add purpose new employee(sort)

--- assets/script/gpform/GP-RB/show.js
+++ assets/script/gpform/GP-RB/show.js
@@ -40,8 +40,10 @@ $(async function () {
 
   const purpose = await getData();
   console.log(purpose);
+  const sortpurpose = purpose.sort((a,b) => a.PURPOSE_ID-b.PURPOSE_ID);
 
-  const Purposedata = purpose
+
+  const Purposedata = sortpurpose
     .map((a) => {
       const otherSelect = `<input type="text"
                             class="input input-sm input-ghost w-full rounded-none border-b border-base-300 focus:border-primary focus:bg-base-200/50 px-1"
@@ -53,7 +55,7 @@ $(async function () {
                                     [--chkbg:var(--bc)] [--chkfg:var(--b1)] req" value="${a.PURPOSE_ID}"
                                     id="purpose_${a.PURPOSE_ID}" >
                                 <span>${a.PURPOSE_TH}/${a.PURPOSE_EN}</span>
-                                ${a.PURPOSE_ID == 4 ? otherSelect : ""}
+                                ${a.PURPOSE_ID == 5 ? otherSelect : ""}
                             </label>`;
     })
     .join("");
