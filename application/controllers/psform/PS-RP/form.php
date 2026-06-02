f2f577534a19aeddcfe1177ee6db0d963b46a24f
Chalormsak S. <s.chalormsak@gmail.com>
2026-06-02 12:16:44 +0700

    feat: init project PS-RP: return part to WHI

---
+++ application/controllers/psform/PS-RP/form.php
@@ -0,0 +1,43 @@
+<?php
+use GuzzleHttp\Client;
+defined('BASEPATH') OR exit('No direct script access allowed');
+require_once APPPATH.'controllers/api/webform/form.php';
+require_once APPPATH.'controllers/api/webform/flow.php';
+require_once APPPATH.'controllers/api/webform/formmst.php';
+class form extends MY_Controller{
+    use formApi, flow, formmst;
+    protected $title;
+    protected $client;
+    protected $formname = 'PS-RR';
+    protected $formno = '4';
+    protected $formorg = '050501';
+    protected $formyear = '26';
+
+    function __construct(){
+		parent::__construct();
+        $this->client = new Client(['verify' => false]);
+    }
+
+    public function main(){
+        $data = array(
+            'NFRMNO' => $this->formno,
+            'VORGNO' => $this->formorg,
+            'CYEAR' => $this->formyear,
+            'empno' => isset($_GET["empno"]) ? $_GET['empno'] : '' ,
+        );
+
+        if(isset($_GET["runNo"]) && $_GET["runNo"] != "") {
+            $data['CYEAR2'] = $_GET['y2'];
+            $data['NRUNNO'] = $_GET['runNo'];
+            $data['EMPNO'] = $data['empno'];
+            $this->views('psform/PS-RR/show', $data);
+        }else {
+            $this->views('psform/PS-RR/create', $data);
+        }
+    }
+
+    public function report(){
+        $data = array();
+        $this->views('psform/PS-RR/report', $data);
+    }
+}
