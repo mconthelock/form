e5586e218a0989dc17786aaa9ea9adce6a95c612
ksatidpaisankul <ksatidpaisankul@gmail.com>
2026-07-17 09:47:08 +0700

    PUR-EVA

--- assets/script/purform/PUR-EVA/create.js
+++ assets/script/purform/PUR-EVA/create.js
@@ -35,7 +35,7 @@ import {
 } from '../PUR-NVF/formManager';
 import { concernManager, currencyManager } from './formManager';
 import { showMessage } from '@amec/webasset/utils';
-import { getOrganize } from '../../finform/FIN-PCK/locmst';
+import { getOrganize } from '../../finform/FIN-PCK/dataloc';
 
 // select2();
 var tableSearch, purformdata, columnPurNVF;
