cf5a748cf9277a59c443d3cb4cb74798880a6eec
punnawichs <punnawichs@mitsubishielevatorasia.co.th>
2026-07-17 13:36:09 +0700

    Revise create, show, report page

--- application/views/psform/PS-DLC/create.blade.php
+++ application/views/psform/PS-DLC/create.blade.php
@@ -54,7 +54,7 @@
         <div class="bg-primary text-primary-content p-6">
             <h1
                 class="text-xl md:text-3xl font-bold text-center text-white flex flex-wrap items-center justify-center gap-2">
-                <svg class="h-9 w-9 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                <svg class="h-9 w-9 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
                     width="24" height="24" fill="none" viewBox="0 0 24 24">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                         d="M15 4h3a1 1 0 0 1 1 1v15a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1V5a1 1 0 0 1 1-1h3m0 3h6m-3 5h3m-6 0h.01M12 16h3m-6 0h.01M10 3v4h4V3h-4Z" />
@@ -129,13 +129,7 @@
                             class="input hidden input-bordered transition-all duration-200 focus:input-primary w-full req"
                             id="schd_p" readonly />
                         <button class="btn btn-neutral" type="button" id="openDatePicker">
-                            <i class="fi fi-rr-calendar"><svg class="w-6 h-6 text-gray-800 dark:text-white"
-                                    aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                    fill="none" viewBox="0 0 24 24">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                        stroke-width="2"
-                                        d="M4 10h16m-8-3V4M7 7V4m10 3V4M5 20h14a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1H5a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1Zm3-7h.01v.01H8V13Zm4 0h.01v.01H12V13Zm4 0h.01v.01H16V13Zm-8 4h.01v.01H8V17Zm4 0h.01v.01H12V17Zm4 0h.01v.01H16V17Z" />
-                                </svg>
+                            <i class="fi fi-rr-calendar">
                             </i>
                         </button>
                         <input type="hidden" id="selectedDate" name="CHANGE_DATE" value="" class="fdate w-0" />
--- application/views/psform/PS-DLC/show.blade.php
+++ application/views/psform/PS-DLC/show.blade.php
@@ -54,7 +54,7 @@
         <div class="bg-primary text-primary-content p-6">
             <h1
                 class="text-xl md:text-3xl font-bold text-center text-white flex flex-wrap items-center justify-center gap-2">
-                <svg class="h-9 w-9 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                <svg class="h-9 w-9 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
                     width="24" height="24" fill="none" viewBox="0 0 24 24">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                         d="M15 4h3a1 1 0 0 1 1 1v15a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1V5a1 1 0 0 1 1-1h3m0 3h6m-3 5h3m-6 0h.01M12 16h3m-6 0h.01M10 3v4h4V3h-4Z" />
