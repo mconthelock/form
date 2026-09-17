21e9eab0bddf504ff347dd38e58db993596a804e
BB8 <chalorms@MitsubishiElevatorAsia.co.th>
2026-09-17 19:41:42 +0700

    fix: improve URL construction in setFormList to handle query parameters correctly

--- assets/script/form/create.js
+++ assets/script/form/create.js
@@ -254,6 +254,7 @@ async function setFormList(data, group) {
     const user = $('#user-login').attr('empno');
     const hash = CryptoJS.MD5(user);
     const bp = document.URL;
+
     // &empnolv=${hash.toString().toUpperCase()}
     const filtered = data.filter((item) => item.VDIR == group.VGROUP);
     if (!filtered.length) {
@@ -262,7 +263,8 @@ async function setFormList(data, group) {
     let str = `
         <li class="list-row list-group p-4 pb-2 text-xl text-primary font-black tracking-wide">${group.VGROUPNAME}</li>`;
     filtered.forEach((item) => {
-        str += `<li class="list-row list-data border border-white cursor-pointer hover:bg-base-300 hover:border-slate-300 create-form-detail" data-url="${item.VFORMPAGE}?no=${item.NNO}&orgNo=${item.VORGNO}&y=${item.CYEAR}&empno=${user}&bp=${bp}" data-name="${item.VNAME}" data-desc="${item.VDESC == null ? '' : item.VDESC}" data-code="${item.VANAME}">
+        const uri = item.VFORMPAGE.includes('?') ? '&' : '?';
+        str += `<li class="list-row list-data border border-white cursor-pointer hover:bg-base-300 hover:border-slate-300 create-form-detail" data-url="${item.VFORMPAGE}${uri}no=${item.NNO}&orgNo=${item.VORGNO}&y=${item.CYEAR}&empno=${user}&bp=${bp}" data-name="${item.VNAME}" data-desc="${item.VDESC == null ? '' : item.VDESC}" data-code="${item.VANAME}">
             <div class="text-4xl font-thin opacity-30 tabular-nums min-w-37">${item.VANAME}</div>
             <div class="list-col-grow">
                 <div>${item.VNAME}</div>
